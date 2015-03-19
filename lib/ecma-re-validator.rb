3c1f3cec6a1d271d836430e35d023ecb645e0c7b
Garen Torikian <gjtorikian@gmail.com>
2015-03-18 18:14:13 -0700

    Immensely simplify token detection

--- lib/ecma-re-validator.rb
+++ lib/ecma-re-validator.rb
@@ -7,6 +7,22 @@ require 'regexp_parser'
 
 module EcmaReValidator
 
+  # JS doesn't have Unicode matching
+  UNICODE_CHARACTERS = Regexp::Syntax::Token::UnicodeProperty::All
+
+  INVALID_REGEXP = [
+    # JS doesn't have \A or \Z
+    :bos, :eos_ob_eol,
+    # JS doesn't have lookbehinds
+    :lookbehind, :nlookbehind,
+    # JS doesn't have atomic grouping
+    :atomic,
+    # JS doesn't have possesive quantifiers
+    :zero_or_one_possessive, :zero_or_more_possessive, :one_or_more_possessive
+  ]
+
+  INVALID_TOKENS = INVALID_REGEXP + UNICODE_CHARACTERS
+
   def self.valid?(input)
     if input.is_a? String
       begin
@@ -20,20 +36,8 @@ module EcmaReValidator
 
     tokens = Regexp::Scanner.scan(input)
 
-    items = []
-    tokens.each { |a| items << a[1] }
+    items = tokens.map { |t| t[1] }
 
-    items.flatten!
-    items.none? do |i|
-      # JS doesn't have \A or \Z
-      i == :bos || i == :eos_ob_eol || \
-      # JS doesn't have lookbehinds
-      i == :lookbehind || i == :nlookbehind || \
-      # JS doesn't have atomic groping
-      i == :atomic || \
-      # JS doesn't have possesive quantifiers
-      i == :zero_or_one_possessive || i == :zero_or_more_possessive || \
-      i == :one_or_more_possessive
-    end
+    (items & INVALID_TOKENS).empty?
   end
 end
